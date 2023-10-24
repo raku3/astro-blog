f5accc8460e08b4382f78b2639c15f1a6e02f3ca
raku3 <binntyoutan@gmail.com>
2023-10-24 23:53:02 +0900

    fix-b

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,10 @@
-import { defineConfig } from "astro/config";
-import AutoImport from "astro-auto-import";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [
-    AutoImport({
-      imports: [
-        // Import a component’s default export
-        // generates:
-        // import A from './src/components/A.astro';
-        "./src/components/A.astro",
-
-        {
-          // Explicitly alias a default export
-          // generates:
-          // import { default as B } from './src/components/B.astro';
-          "./src/components/B.astro": [["default", "B"]],
-
-          // Import a module’s named exports
-          // generates:
-          // import { Tweet, YouTube } from 'astro-embed';
-          "astro-embed": ["Tweet", "YouTube"]
-        }
-      ]
-    }),
-
-    // Make sure the MDX integration is included AFTER astro-auto-import
-    mdx()
-  ]
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
